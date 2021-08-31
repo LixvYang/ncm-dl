114297d405a9de2db88983ccc38a5813a036ade3
lixin <2690688423@qq.com>
2021-08-31 20:15:35 +0800

    update

--- netease/crypto.go
+++ netease/crypto.go
@@ -5,8 +5,19 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/base64"
+	"fmt"
+	"math/big"
 	"math/rand"
+	"ncm-dl/utils"
 	"time"
+)
+
+const (
+	Base62                      = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	PresetKey                   = "0CoJUm6Qyw8W8jud"
+	IV                          = "0102030405060708"
+	DefaultRSAPublicKeyModulus  = "e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7"
+	DefaultRSAPublicKeyExponent = 0x10001
 )
 
 func Encrypt(origData []byte) (params,encSecKey string,err error) {
@@ -17,12 +28,29 @@ func Encrypt(origData []byte) (params,encSecKey string,err error) {
 
 	secKey := createSecretKey(16,Base62) 
 
+	enc2,err := aesCBCEncrypt([]byte(enc1),secKey,[]byte(IV))
+	if err != nil {
+		return
+	}
+
+	params, encSecKey = enc2, rsaEncrypt(utils.BytesReverse(secKey),DefaultRSAPublicKeyModulus, DefaultRSAPublicKeyExponent)
+	return
+}
+
+func rsaEncrypt(origData []byte,modulus string,exponent int64) string {
+	bigOrigData,bigModulus := new(big.Int),new(big.Int)
+
+	bigOrigData.SetBytes(origData)
+	bigModulus.SetString(modulus,16)
+	
+	return fmt.Sprintf("%0256x", bigOrigData.Exp(bigOrigData, big.NewInt(exponent), bigModulus))
+
 }
 
 func createSecretKey(size int,charset string) []byte {
 	secKey := make([]byte,size)
 	n := len(charset)
-	
+
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i := range secKey {
 		secKey[i] = charset[r.Intn(n)]
--- utils/path.go
+++ utils/path.go
@@ -25,3 +25,10 @@ func BuildPathIfNotExist(path string) error {
 		return err
 	}
 }
+
+func BytesReverse(b []byte) []byte {
+	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
+	return b
+}
